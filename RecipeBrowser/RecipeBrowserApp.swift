fd708a5c0f2278347c63b9a620b176bc91059237
Iain Lee <iainleeksi@gmail.com>
2024-07-18 14:41:17 -0400

    Initial Commit

---
+++ RecipeBrowser/RecipeBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBrowserApp.swift
+//  RecipeBrowser
+//
+//  Created by Iain Lee on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
