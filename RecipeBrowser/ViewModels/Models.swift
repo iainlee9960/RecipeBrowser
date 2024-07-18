a70806cd5e4a239d129d47223fa9235dbaba8313
Iain Lee <iainleeksi@gmail.com>
2024-07-18 15:07:16 -0400

    added logo

--- RecipeBrowser/ViewModels/Models.swift
+++ RecipeBrowser/ViewModels/Models.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Meal: Codable, Identifiable {
     let id: String
@@ -50,4 +51,30 @@ struct MealDetail: Codable {
         }
         self.ingredients = ingredients
     }
+}
+
+extension Color {
+    //static let mainColor = Color(UIColor(red: 118/255, green: 221/255, blue: 253/255, alpha: 1)) // OG Light Blue
+    // i think this is the most appetizing color by far
+    static let mainColor = Color(UIColor(red: 250/255, green: 100/255, blue: 100/255, alpha: 1)) // Pastel Red
+        
+    // similar to Light Blue, its a food app i don't really like it
+    //static let mainColor = Color(UIColor(red: 118/255, green: 253/255, blue: 221/255, alpha: 1))
+    //static let mainColor = Color(UIColor(red: 118/255, green: 190/255, blue: 253/255, alpha: 1))
+    
+    // similar to Red, also good, but not good enough
+    //static let mainColor = Color(UIColor(red: 253/255, green: 150/255, blue: 118/255, alpha: 1))
+    //static let mainColor = Color(UIColor(red: 253/255, green: 118/255, blue: 150/255, alpha: 1))
+    
+    // Complementary colors
+    //static let mainColor = Color(UIColor(red: 253/255, green: 118/255, blue: 172/255, alpha: 1)) // Pink
+    //static let mainColor = Color(UIColor(red: 100/255, green: 250/255, blue: 250/255, alpha: 1)) // Light Cyan
+    
+    // Triadic colors
+    //static let mainColor = Color(UIColor(red: 253/255, green: 253/255, blue: 118/255, alpha: 1))
+    //static let mainColor = Color(UIColor(red: 221/255, green: 118/255, blue: 253/255, alpha: 1))
+    
+    // some extra colors, could use for some accenting
+    //static let mainColor = Color(UIColor(red: 100/255, green: 250/255, blue: 100/255, alpha: 1))
+    //static let mainColor = Color(UIColor(red: 100/255, green: 100/255, blue: 250/255, alpha: 1))
 }
