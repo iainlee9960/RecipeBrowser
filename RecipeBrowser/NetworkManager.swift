67a3b5877601301a358c388a70c0ef342d9beba9
Iain Lee <iainleeksi@gmail.com>
2024-07-18 15:13:01 -0400

    filter out null values

--- RecipeBrowser/NetworkManager.swift
+++ RecipeBrowser/NetworkManager.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-import Foundation
-
 class NetworkManager {
     static let shared = NetworkManager()
     private let baseURL = "https://themealdb.com/api/json/v1/1/"
@@ -19,14 +17,18 @@ class NetworkManager {
         guard let url = URL(string: "\(baseURL)filter.php?c=Dessert") else { fatalError("Invalid URL") }
         let (data, _) = try await URLSession.shared.data(from: url)
         let meals = try JSONDecoder().decode(MealResponse.self, from: data)
-        return meals.meals.sorted(by: { $0.name < $1.name })
+        // Filter out any meals with null or empty names
+        return meals.meals.filter { !$0.name.isEmpty }.sorted(by: { $0.name < $1.name })
     }
 
     func fetchMealDetails(mealID: String) async throws -> MealDetail {
         guard let url = URL(string: "\(baseURL)lookup.php?i=\(mealID)") else { fatalError("Invalid URL") }
         let (data, _) = try await URLSession.shared.data(from: url)
         let mealDetails = try JSONDecoder().decode(MealDetailResponse.self, from: data)
-        return mealDetails.meals.first!
+        guard let mealDetail = mealDetails.meals.first else {
+            throw NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No meal detail found"])
+        }
+        return mealDetail
     }
 }
 
